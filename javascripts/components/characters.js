d31b6adcf9977b65134833fcf51f561797b2d895
Zoe Ames <zoeames@gmail.com>
2018-10-01 20:29:58 -0500

    added dat

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -1,1 +1,8 @@
-console.log('characters');
+const characters = [
+  {name: 'John Snow', house: 'Stark', imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553'},
+  {name: 'Daenerys Targaryen', house: 'Targaryen', imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/5f/Daenerys_Dragonpit.jpg/revision/latest?cb=20171015095128'},
+  {name: 'Cersei Lannister', house: 'Lannister', imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/c/c3/Profile-CerseiLannister.png/revision/latest/scale-to-width-down/323?cb=20170828071355'},
+  {name: 'Tyrion Lannister', house: 'Lannister', imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/58/Tyrion_main_s7_e6.jpg/revision/latest?cb=20170818050344'}
+];
+
+console.log('characters', characters);
